ac2570106227383c0b4130f3d0223cfd785af2f4
Joanna Wu <joanna_wu@live.com>
2017-10-22 11:30:32 +1100

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const http = require('http');
 const path = require('path');
 
 const port = 4001;
-const staticFileHandler = http.fileHandler(path.join(__dirname, 'docs'));
+const staticFileHandler = http.fileHandler(path.join(__dirname, '.'));
 
 const server = new http.Server(port, staticFileHandler);
 console.log(`preview server running on http://127.0.0.1:${port}`);
